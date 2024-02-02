db5fd616bbbafaa43cee6168fc82b52f509f8568
AlexanderFKratz <49705346+afkratz@users.noreply.github.com>
2024-02-01 17:43:51 -0800

    Rewritten pipeline to allow us to identify off-targets

--- 04_make_db.cmd
+++
@@ -1,2 +0,0 @@
-makeblastdb -in ../genomic_targets.fasta -dbtype nucl -out ../genomic_targets
-makeblastdb -in ../linkers.fasta -dbtype nucl -out ../linkers
--- 05_run_blast.cmd
+++
@@ -1,5 +0,0 @@
-echo "Running blast against linkers" 
-blastn -query ../reads_combined.fasta -db ../linkers -out ../linker_alignments.txt -task blastn-short -max_target_seqs 1 -outfmt 6 -num_threads 6
-
-echo "Running blast against targets" 
-blastn -query ../reads_combined.fasta -db ../genomic_targets -out ../genomic_target_alignments.txt -task blastn-short -max_target_seqs 1 -outfmt 6 -num_threads 6
